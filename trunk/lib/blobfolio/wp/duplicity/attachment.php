3f9c0ec88bbcde6cf047701cdd91c96969c9621b
Josh Stoik <josh@blobfolio.com>
2017-09-24 15:15:58 -0700

    deduplication

--- trunk/lib/blobfolio/wp/duplicity/attachment.php
+++ trunk/lib/blobfolio/wp/duplicity/attachment.php
@@ -11,11 +11,23 @@
 namespace blobfolio\wp\duplicity;
 
 use \blobfolio\wp\duplicity\vendor\common;
+use \WP_CLI;
+use \WP_CLI\Utils;
 
 class attachment {
 
+	const META_KEY = '_duplicity_id';
+
+	const SEARCH_REPLACE = array(
+		'return'=>true,
+		'parse'=>'STDOUT',
+		'launch'=>false,
+		'exit_error'=>false,
+	);
+
 	protected static $attachments;
 	protected static $checksums;
+	protected static $database;
 	protected static $upload_dir;
 	protected static $upload_url;
 
@@ -95,7 +107,7 @@ class attachment {
 	 */
 	protected static function load_checksums($refresh=false) {
 		if ($refresh || !is_array(static::$checksums)) {
-			static::load_attachments();
+			static::load_attachments($refresh);
 			static::load_paths();
 			static::$checksums = array();
 
@@ -129,23 +141,21 @@ class attachment {
 	}
 
 	/**
-	 * Get Duplicate Posts
+	 * Get Linted Attachments
 	 *
-	 * You'd be surprised, but some plugins duplicate post entries
-	 * without duplicating files. This is stupid and dangerous since
-	 * deleting one would delete all.
+	 * Return a list of attachments that have already undergone linting.
 	 *
 	 * @param bool $refresh Refresh.
 	 * @return array Post IDs.
 	 */
-	public static function get_duplicate_posts($refresh=false) {
+	public static function get_linted_attachments($refresh=false) {
 		$out = array();
 
 		// Make a copy of our attachments list because this process is
 		// destructive.
 		$attachments = static::get_attachments($refresh);
 		if (!count($attachments)) {
-			return array();
+			return $out;
 		}
 
 		// Are there any duplicates? Let's start by counting values.
@@ -163,6 +173,8 @@ class attachment {
 				$out[$k][] = $post_id;
 				unset($attachments[$post_id]);
 			}
+
+			sort($out[$k]);
 		}
 
 		ksort($out);
@@ -182,7 +194,7 @@ class attachment {
 
 		$checksums = static::get_checksums($refresh);
 		if (!count($checksums)) {
-			return array();
+			return $out;
 		}
 
 		foreach ($checksums as $k=>$v) {
@@ -195,5 +207,357 @@ class attachment {
 
 		ksort($out);
 		return $out;
+	}
+
+	/**
+	 * Get Sister Files
+	 *
+	 * Find all files associated with a particular attachment.
+	 *
+	 * @param string $source File source (relative).
+	 * @return array Files.
+	 */
+	protected static function get_sister_files($source='') {
+		static::load_paths();
+		common\ref\cast::to_string($source, true);
+		$out = array();
+
+		if (!$source) {
+			return $out;
+		}
+		$subdir = trailingslashit(dirname($source));
+		$dir = static::$upload_dir . $subdir;
+		if (!@is_dir($dir)) {
+			return $out;
+		}
+
+		$source = basename($source);
+		$filename = pathinfo($source, PATHINFO_FILENAME);
+		$ext = pathinfo($source, PATHINFO_EXTENSION);
+		if (!$filename || !$ext) {
+			return $out;
+		}
+
+		$pattern = preg_quote($filename, '/') . '(\-\d+x\d+)?\.' . preg_quote($ext, '/');
+
+		if ($handle = opendir($dir)) {
+			while (false !== ($file = readdir($handle))) {
+				if (('.' === $file) || ('..' === $file)) {
+					continue;
+				}
+
+				if (preg_match("/^$pattern$/", $file) && @is_file($dir . $file)) {
+					$out[] = $subdir . $file;
+				}
+			}
+
+			closedir($handle);
+		}
+
+		return $out;
+	}
+
+	/**
+	 * Quick MySQL Search
+	 *
+	 * This checks to see whether a substring can be found in any
+	 * prefixed table. This allows us to avoid expensive search/replace
+	 * calls for items that aren't referenced anywhere.
+	 *
+	 * @param string $str String.
+	 * @return bool True/false.
+	 */
+	protected static function mysql_search($str='') {
+		common\ref\cast::to_string($str, true);
+		common\ref\mb::trim($str);
+		if (!$str) {
+			return false;
+		}
+
+		global $wpdb;
+		$str = esc_sql($wpdb->esc_like($str));
+
+		// Do we need to build the schema?
+		if (!is_array(static::$database)) {
+			static::$database = array();
+
+			// Pull tables, listing anything *post* first since that
+			// will be the most likely place to find a result.
+			$dbResult = $wpdb->get_results("
+				SELECT `table_name`
+				FROM information_schema.tables
+				WHERE
+					`table_type` = 'BASE TABLE' AND
+					`table_schema` = '" . DB_NAME . "' AND
+					`table_name` LIKE '{$wpdb->prefix}%'
+				ORDER BY
+					(`table_name` LIKE '%post%') DESC,
+					`table_name` ASC
+			", ARRAY_N);
+			if (is_array($dbResult) && count($dbResult)) {
+				foreach ($dbResult as $Row) {
+					static::$database[$Row[0]] = array();
+
+					// Now pull columns. We only care about (var)char
+					// and text since we're searching for a string and
+					// WP doesn't go binary or anything like that.
+					$dbResult2 = $wpdb->get_results("
+						SHOW COLUMNS FROM `{$Row[0]}`
+					", ARRAY_N);
+					if (is_array($dbResult2) && count($dbResult2)) {
+						foreach ($dbResult2 as $Row2) {
+							if (
+								(false !== strpos($Row2[1], 'char(')) ||
+								(false !== strpos($Row2[1], 'text('))
+							) {
+								static::$database[$Row[0]][] = $Row2[0];
+							}
+						}
+					}
+
+					if (!count(static::$database[$Row[0]])) {
+						unset(static::$database[$Row[0]]);
+					}
+				}
+			}
+		}
+
+		// We couldn't build the table. Who knows?
+		if (!count(static::$database)) {
+			return true;
+		}
+
+		// All right, let's run some queries!
+		foreach (static::$database as $table=>$columns) {
+			$conds = array();
+			foreach ($columns as $v) {
+				$conds[] = "`$v` LIKE '%$str%'";
+			}
+			$conds = implode(' OR ', $conds);
+
+			$hits = (int) $wpdb->get_var("SELECT COUNT(*) FROM `$table` WHERE $conds");
+			if ($hits) {
+				return true;
+			}
+		}
+
+		return false;
+	}
+
+	/**
+	 * Deduplicate Files
+	 *
+	 * This will repoint duplicate attachment posts to the original
+	 * source file, remove its files from the system, and search-replace
+	 * references to the file paths.
+	 *
+	 * ID-based references will remain, but since the post's attachment
+	 * reference has been updated, should now resolve to the original
+	 * image.
+	 *
+	 * @return array Post IDs.
+	 */
+	public static function deduplicate_files() {
+		$out = array(
+			'deleted'=>array(),
+			'posts'=>array(),
+			'saved'=>array(),
+		);
+
+		// First, check to see if there are any duplicate files.
+		$files = static::get_duplicate_files();
+		if (!count($files)) {
+			return $out;
+		}
+
+		// Grab a list of what we've already linted.
+		$linted = static::get_linted_attachments(true);
+		require_once(ABSPATH . 'wp-admin/includes/image.php');
+		global $wpdb;
+
+		// Take a quick break to make sure they really want to proceed.
+		WP_CLI::warning(
+			__('Files with duplicates:', 'duplicity') . ' ' . count($files)
+		);
+		WP_CLI::confirm(
+			__('Really proceed with deduplication? You should back up files and data before proceeding.', 'duplicity')
+		);
+
+		// Loop it.
+		$progress = Utils\make_progress_bar('Deduplicating attachments.', count($files));
+		foreach ($files as $k=>$v) {
+			$progress->tick();
+
+			$primary = '';
+			$dupes = array();
+			$update_posts = array();
+			$update_files = array();
+			$in_lint = 0;
+
+			foreach ($v as $v2) {
+				if (!$primary && isset($linted[$v2])) {
+					$primary = $v2;
+					$in_lint++;
+				}
+				else {
+					$dupes[] = $v2;
+					if (isset($linted[$v2])) {
+						$in_lint++;
+					}
+				}
+			}
+
+			// If we don't have a primary yet, just pluck the first from
+			// the list.
+			if (!$primary) {
+				$primary = array_shift($dupes);
+			}
+
+			// Go through our data and find all files (duplicate only)
+			// and all affected post IDs (including primary).
+			$attachments = static::get_attachments();
+			foreach ($dupes as $v2) {
+				if ($v2 !== $primary) {
+					$update_files += static::get_sister_files($v2);
+				}
+
+				while (false !== ($post_id = array_search($v2, $attachments, true))) {
+					$update_posts[] = $post_id;
+					unset($attachments[$post_id]);
+				}
+			}
+			while (false !== ($post_id = array_search($primary, $attachments, true))) {
+				$update_posts[] = $post_id;
+				unset($attachments[$post_id]);
+			}
+			$update_files = array_values(array_unique($update_files));
+			$update_posts = array_values(array_unique($update_posts));
+
+			// The main ID is the oldest. This may or may not correspond
+			// to the file we've identified as primary, but that doesn't
+			// really matter.
+			$primary_id = $update_posts[0];
+
+			// Step one: update file entries for post/postmeta.
+			$cond = implode(',', $update_posts);
+			$wpdb->query("
+				UPDATE `{$wpdb->posts}`
+				SET `guid`='" . esc_sql(static::$upload_url . $primary) . "'
+				WHERE `ID` IN ($cond)
+			");
+			$wpdb->query("
+				UPDATE `{$wpdb->postmeta}`
+				SET `meta_value`='" . esc_sql($primary) . "'
+				WHERE
+					`meta_key`='_wp_attached_file' AND
+					`post_id` IN ($cond)
+			");
+
+			// Step two: update attachment metadata.
+			$attach_data = wp_generate_attachment_metadata($primary_id, static::$upload_dir . $primary);
+			foreach ($update_posts as $post_id) {
+				wp_update_attachment_metadata($post_id, $attach_data);
+				$out['posts'][] = $post_id;
+			}
+
+			// Step three: search/replace content for file references
+			// and delete files as we go.
+
+			// Pull apart our primary file.
+			$primary_subdir = trailingslashit(dirname($primary));
+			$primary_filename = pathinfo($primary, PATHINFO_FILENAME);
+			$primary_ext = pathinfo($primary, PATHINFO_EXTENSION);
+
+			// Loop over the dupes.
+			foreach ($update_files as $v2) {
+				$old_filename = pathinfo($v2, PATHINFO_FILENAME);
+				preg_match('/\-\d+x\d+$/', $old_filename, $match);
+				$size = count($match) ? $match[0] : '';
+
+				// Build our search/replace pair.
+				$from = str_replace("'", '', $v2);
+				$to = str_replace("'", '', $primary_subdir . $primary_filename . $size . '.' . $primary_ext);
+				if (!@file_exists(static::$upload_dir . $to)) {
+					$to = $primary;
+				}
+
+				// Let WP-CLI handle replacements its usual way. We're
+				// using ::launch_self instead of ::runcommand because
+				// that seems to be the only way to prevent it from
+				// polluting STDOUT.
+				if (static::mysql_search($v2)) {
+					WP_CLI::launch_self(
+						'search-replace',
+						array($from, $to),
+						array('all-tables-with-prefix'=>true),
+						true,
+						false,
+						array('quiet'=>true)
+					);
+				}
+
+				// Now we can remove the old file.
+				@unlink(static::$upload_dir . $v2);
+				$out['deleted'][] = $v2;
+			}
+
+			// Note which files we're keeping.
+			$saved = static::get_sister_files($primary);
+			foreach ($saved as $v2) {
+				$out['saved'][] = $v2;
+			}
+
+			// Reload the linted data because we've had to merge two or
+			// more linted groups.
+			if ($in_lint > 1) {
+				$linted = static::get_linted_attachments(true);
+			}
+		}
+		$progress->finish();
+
+		return $out;
+	}
+
+	/**
+	 * Rebuild Deduplication Attachment Meta
+	 *
+	 * To lighten subsequent search operations, each deduplicated
+	 * attachment will receive a special postmeta entry.
+	 *
+	 * @return bool True.
+	 */
+	public static function regenerate_postmeta() {
+		global $wpdb;
+
+		// To start with, remove all existing entries.
+		$wpdb->delete(
+			$wpdb->postmeta,
+			array('meta_key'=>static::META_KEY),
+			'%s'
+		);
+
+		$inserts = array();
+		$linted = static::get_linted_attachments(true);
+
+		// Build new meta_values.
+		foreach ($linted as $v) {
+			$primary = $v[0];
+			foreach ($v as $v2) {
+				$inserts[] = "($v2,'" . static::META_KEY . "',$primary)";
+			}
+		}
+
+		// Insert them in chunks for performance reasons.
+		if (count($inserts)) {
+			$inserts = array_chunk($inserts, 250);
+			foreach ($inserts as $v) {
+				$wpdb->query("
+					INSERT INTO `{$wpdb->postmeta}` (`post_id`, `meta_key`, `meta_value`)
+					VALUES " . implode(', ', $v)
+				);
+			}
+		}
+
+		return true;
 	}
 }
--- trunk/lib/blobfolio/wp/duplicity/cli.php
+++ trunk/lib/blobfolio/wp/duplicity/cli.php
@@ -26,19 +26,13 @@ class cli extends \WP_CLI_Command {
 	/**
 	 * List Duplicate Attachments
 	 *
-	 * There are two senses in which an attachment is a "duplicate":
-	 * 1) The wp_posts entry has been duplicated;
-	 * 2) The same file has been independently uploaded more than once;
+	 * The same file might be uploaded to WordPress more than once.
+	 * This will scan the file system to identify duplicates.
 	 *
 	 * ## OPTIONS
 	 *
-	 * [--include-posts]
-	 * : Search wp_posts for duplicated entries.
-	 * Default: True
-	 *
-	 * [--include-files]
-	 * : Search the file system for identical files.
-	 * Default: True.
+	 * [--linted]
+	 * : Show entries that have already been deduplicated.
 	 *
 	 * @param array $args Not used.
 	 * @param array $assoc_args Flags.
@@ -47,8 +41,10 @@ class cli extends \WP_CLI_Command {
 	 * @subcommand list
 	 */
 	public function _list($args, $assoc_args=array()) {
-		$include_posts = !!Utils\get_flag_value($assoc_args, 'include-posts', true);
-		$include_files = !!Utils\get_flag_value($assoc_args, 'include-files', true);
+		$include_posts = !!Utils\get_flag_value($assoc_args, 'linted', false);
+
+		// Install the MU plugin script.
+		plugin::install();
 
 		$translated = array(
 			'File'=>__('File', 'duplicity'),
@@ -61,7 +57,7 @@ class cli extends \WP_CLI_Command {
 		if ($include_posts) {
 			WP_CLI::log('');
 
-			$posts = attachment::get_duplicate_posts();
+			$posts = attachment::get_linted_attachments();
 			if (count($posts)) {
 				$headers = array(
 					$translated['File'],
@@ -80,53 +76,130 @@ class cli extends \WP_CLI_Command {
 				}
 
 				Utils\format_items('table', $data, $headers);
-				WP_CLI::warning(
-					__('Duplicated attachment posts:', 'duplicity') . " $total"
-				);
-			}
-			else {
 				WP_CLI::success(
-					__('No file attachment posts have been duplicated.', 'duplicity')
+					__('Linted attachments:', 'duplicity') . " $total"
 				);
 			}
 		}
 
 		// Search for duplicated files.
-		if ($include_files) {
-			WP_CLI::log('');
+		WP_CLI::log('');
 
-			$files = attachment::get_duplicate_files();
-			if (count($files)) {
-				$headers = array(
-					$translated['MD5'],
-					$translated['Files'],
+		$files = attachment::get_duplicate_files();
+		if (count($files)) {
+			$headers = array(
+				$translated['MD5'],
+				$translated['Files'],
+			);
+
+			$data = array();
+			$total = 0;
+			foreach ($files as $k=>$v) {
+				$data[] = array(
+					$translated['MD5']=>$k,
+					$translated['Files']=>implode('; ', $v),
 				);
 
-				$data = array();
-				$total = 0;
-				foreach ($files as $k=>$v) {
-					$data[] = array(
-						$translated['MD5']=>$k,
-						$translated['Files']=>implode('; ', $v),
-					);
-
-					$total += (count($v) - 1);
-				}
-
-				Utils\format_items('table', $data, $headers);
-				WP_CLI::warning(
-					__('Duplicated file uploads:', 'duplicity') . " $total"
-				);
+				$total += (count($v) - 1);
 			}
-			else {
-				WP_CLI::success(
-					__('No duplicate files have been uploaded.', 'duplicity')
-				);
-			}
+
+			Utils\format_items('table', $data, $headers);
+			WP_CLI::warning(
+				__('Duplicated file uploads:', 'duplicity') . " $total"
+			);
+		}
+		else {
+			WP_CLI::success(
+				__('No duplicate files have been uploaded.', 'duplicity')
+			);
 		}
 
 		WP_CLI::log('');
 		return true;
 	}
 
+	/**
+	 * Deduplicate
+	 *
+	 * Remove and relink duplicate file attachments.
+	 *
+	 * ## OPTIONS
+	 *
+	 * @return bool True/false.
+	 */
+	public function deduplicate() {
+		$results = attachment::deduplicate_files();
+
+		// Install the MU plugin script.
+		plugin::install();
+
+		if (!$results['posts']) {
+			WP_CLI::success(
+				__('No attachments needed deduplicating.', 'duplicity')
+			);
+			return true;
+		}
+
+		WP_CLI::success(
+			__('Affected posts:', 'duplicity') . ' ' . count($results['posts'])
+		);
+		WP_CLI::success(
+			__('Files removed:', 'duplicity') . ' ' . count($results['deleted'])
+		);
+
+		attachment::regenerate_postmeta();
+
+		return true;
+	}
+
+	/**
+	 * Rebuild Postmeta
+	 *
+	 * To help lighten database queries, deduplicated attachments each
+	 * get a postmeta entry indicating the main source.
+	 *
+	 * This postmeta is updated automatically when other operations are
+	 * performed, however this function can be used on its own if data
+	 * falls out of sync for whatever reason.
+	 *
+	 * @return bool True.
+	 *
+	 * @subcommand regenerate-postmeta
+	 */
+	public function regenerate_postmeta() {
+		attachment::regenerate_postmeta();
+		WP_CLI::success(
+			__('Attachment postmeta has been updated.', 'duplicity')
+		);
+
+		// Install the MU plugin script.
+		plugin::install();
+
+		return true;
+	}
+
+	/**
+	 * Install
+	 *
+	 * This will add a small helper plugin to the WPMU_PLUGIN_DIR. The
+	 * main purpose of this script is improving how deduplicated posts
+	 * and files are handled during delete actions, etc.
+	 *
+	 * @return bool True.
+	 */
+	public function install() {
+		$result = plugin::install();
+
+		if (!$result) {
+			WP_CLI::error(
+				__('The companion plugin could not be installed.', 'duplicity')
+			);
+		}
+
+		WP_CLI::success(
+			__('The companion plugin has been installed.', 'duplicity')
+		);
+
+		return $result;
+	}
 }
